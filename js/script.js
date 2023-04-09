96479d4d998f8af3524d53707a4d9db634b3ba4e
Jasper Mawunyo Kojo Akabua <jasperakabua020@gmail.com>
2023-04-09 21:01:15 +0000

    Places visited updated

--- js/script.js
+++ js/script.js
@@ -1,13 +1,38 @@
 
-    const pic1 = document.querySelector("#gallery");
+//first gallery popup
+    const pic1 = document.querySelector("#gallery1");
     const pictures1 = document.querySelector(".pictures1");
 
     pic1.addEventListener('click', () =>{
     pictures1.classList.toggle('active');
     });
 
-    document.querySelector('.close').onclick = () =>{
+    document.querySelector('.close1').onclick = () =>{
         pictures1.classList.remove('active');
+    };
+
+//second gallery popup
+    const pic2 = document.querySelector("#gallery2");
+    const pictures2 = document.querySelector(".pictures2");
+
+    pic2.addEventListener('click', () =>{
+    pictures2.classList.toggle('active');
+    });
+
+    document.querySelector('.close2').onclick = () =>{
+        pictures2.classList.remove('active');
+    };
+
+//thirdgallery popup
+    const pic3 = document.querySelector("#gallery3");
+    const pictures3 = document.querySelector(".pictures3");
+
+    pic3.addEventListener('click', () =>{
+    pictures3.classList.toggle('active');
+    });
+
+    document.querySelector('.close3').onclick = () =>{
+        pictures3.classList.remove('active');
     };
 
 
@@ -60,9 +85,9 @@ const controlPlaces = document.querySelectorAll('.date');
 
 function ScrollAnimation(){
     gsap.from('.location', {opacity: 0, duration: .2, delay: .2, y:-20})
-    gsap.from('.gallery', {opacity: 0, duration: .3, delay: .1, y:-20})
-    gsap.from('.plimage', {opacity: 0, duration: .3, delay: .1, y:20})
-    gsap.from('.loc-info', {opacity: 0, duration: .4, delay: .4, y:-20})
+    gsap.from('.gallery', {opacity: .5, duration: .3, delay: .1, y:-20})
+    gsap.from('.plimage', {opacity: 1, duration: .3, delay: .1, y:20})
+    gsap.from('.loc-info', {opacity: 0, duration: .2, delay: .2, y:-20})
     
 
 }
