1a6035e4423869cbfd5243abb59b630233a01c99
Jasper Mawunyo Kojo Akabua <jasperakabua020@gmail.com>
2023-04-09 15:02:51 +0000

    New Changes Made

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+    const pic1 = document.querySelector("#gallery");
+    const pictures1 = document.querySelector(".pictures1");
+
+    pic1.addEventListener('click', () =>{
+    pictures1.classList.toggle('active');
+    });
+
+    document.querySelector('.close').onclick = () =>{
+        pictures1.classList.remove('active');
+    };
+
+
+//Video popup
+        let video = document.querySelectorAll('video');
+
+            video.forEach(play => play.addEventListener ('click', () => {
+
+            play.classList.toggle('active');
+
+            if(play.paused){
+                play.play();
+            }else{
+                play.pause();
+                play.currentTime = 0;
+            }
+            
+        }));
+
