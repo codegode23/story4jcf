e417c67700666cc5234249febcad7691da8d8887
Jasper Mawunyo Kojo Akabua <jasperakabua020@gmail.com>
2023-04-12 19:38:30 +0000

    Committing mission and vission

--- js/script.js
+++ js/script.js
@@ -87,7 +87,7 @@ function ScrollAnimation(){
     gsap.from('.location', {opacity: 0, duration: .2, delay: .2, y:-20})
     // gsap.from('.gallery', {opacity: .5, duration: .3, delay: .1, y:-20})
     gsap.from('.plimage', {opacity: 1, duration: .3, delay: .1, y:20})
-    gsap.from('.loc-info', {opacity: 0, duration: .2, delay: .2, y:-20})
+    gsap.from('.loc-info', {opacity: 0, duration: .2, delay: .2, y:20})
     
 
 }
